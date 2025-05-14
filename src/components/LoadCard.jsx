30c7c11e60b6469b6c4a5a7179c111957943934d
Bri Pintens <btpintens@gmail.com>
2025-05-14 15:12:20 -0400

    update loadcard and app for display to show accurately

--- src/components/LoadCard.jsx
+++ src/components/LoadCard.jsx
@@ -55,13 +55,9 @@ const LoadCard = ({ load }) => {
     <>
       <div className="load-card">
         <Link to={`/loads/${load.id}`} className="load-card-link">
-          <h1>{load.name}</h1>
+          <h1>{load.company_name}</h1>
           <h2>Pick-up Location: {load.pickupCity}, {load.pickupState}</h2>
-          <h2>Drop-off Location: {load.dropoffCity}, {load.dropoffState}</h2>
           <h2>Rate: {load.rate}</h2>
-          <h2>Equipment Requirement: {load.equipment}</h2>
-          <h2>Commodity: {load.commodity}</h2>
-          <h2>Delivery Date: {load.deliveryDate}</h2>
         </Link>
 
         {role === 'carrier' && (
